e86d424491af426f6dc57aa46d337d381e9ce7a5
Yasmim Kondo <yasmim.kgtx@gmail.com>
2024-06-19 17:46:56 -0300

    feat: initial commit

---
+++ Views/DiceView.xaml.cs
@@ -0,0 +1,17 @@
+using System;
+using avaliacao2703.Models;
+using avaliacao2703.ViewModels;
+
+namespace avaliacao2703.Views;
+
+public partial class DiceView : ContentPage
+{
+    int numeroAleatorio = 0;
+    public DiceView()
+    {
+        InitializeComponent();
+        BindingContext = new DadoViewModel();
+    }
+
+    
+}
